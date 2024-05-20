be6be57ff866608ce229f877e953313166b53673
Heather Ellsworth <heather.ellsworth@canonical.com>
2024-05-20 14:10:27 -0600

    Remove utils.rs and improve logic in icon_name

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 
 use std::env;
 
-
-//use crate::utils::SomeTrait;
-//mod utils;
-
 #[derive(Debug)]
 struct MyTray {
     selected_option: usize,
@@ -20,16 +16,26 @@ impl ksni::Tray for MyTray {
     fn icon_name(&self) -> String {
         let my_de = env::var("XDG_CURRENT_DESKTOP").expect("error");
         let mut preferred_icon = "Thunderbird.svg";
-        if my_de == "ubuntu:GNOME" {
+        if my_de
+            .replace(":", ";")
+            .split(";")
+            .map(|m| m.to_ascii_lowercase())
+            .any(|e| &e == "gnome")
+        {
             preferred_icon = "tb-symbolic-white.svg";
-        } else if my_de == "KDE" {
-            preferred_icon = "Thunderbird_Logo_Outline-Light.svg"
+        } else if my_de
+            .replace(":", ";")
+            .split(";")
+            .map(|m| m.to_ascii_lowercase())
+            .any(|e| &e == "kde")
+        {
+            preferred_icon = "Thunderbird_Logo_Outline-Light.svg";
         }
-        println!("icon for {} is {}", my_de, preferred_icon);
         preferred_icon.into()
+        
     }
     fn title(&self) -> String {
-        if self.checked { "CHECKED!" } else { "MyTray" }.into()
+        if self.checked { "Thunderbird" } else { "MyTray" }.into()
     }
     // NOTE: On some system trays, `id` is a required property to avoid unexpected behaviors
     fn id(&self) -> String {
@@ -53,28 +59,6 @@ fn main() {
         checked: false,
     });
     let handle = service.handle();
-
-    // I don't know where to send the assets_dir, so that it can be picked up by icon_theme_path
-    // I want the above fn icon_theme_path to have something like:
-    //      assets_dir = utils::get_icons_dir(&struct_for_dir)
-    //      assets_dir.into()
-    // The problem is that I can't seem to pass the value from utils::get_icons_dir because it gets
-    // set in a condition of the match and so is out of scope when I try to return assets_dir at the
-    // end of the function :(
-
-    //let struct_for_dir = utils::RandomStruct {
-    //    directory: PathBuf,
-    //};
-    //let assets_dir = utils::get_icons_dir(&struct_for_dir); // Calls get_icons_dir on the struct defined in utils.rs
-    //let assets_dir = utils::RandomStruct::get_assets_dir();
-    //println!("From main, the assets directory is {:?}", assets_dir);
-
-    //utils::find_my_de();
-
-    // Based on the DE discovered, we could force the Thunderbird-Dark-symbolic.svg.
-    // I don't know how to send whichever svg to icon_name to have something like:
-    //      logo.into()
-    // where logo would point to the svg file, which depends on the DE
 
     service.spawn();
 
